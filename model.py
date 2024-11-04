0cb090756ce6fbef6f81b271f4a1d1b832e1f22a
Paulus Michael Leang <99465357+paullmich28@users.noreply.github.com>
2024-11-04 12:43:56 +0700

    FEAT: Get object

--- model.py
+++ model.py
@@ -1,16 +1,44 @@
 from fastapi import FastAPI, UploadFile, File
 from ultralytics import YOLO
-from collections import Counter
 from PIL import Image
 import uvicorn
 import io
+import socket
 
 app = FastAPI()
 model = YOLO("yolov8n.pt")
 
+hostname = socket.gethostname()
+IP = socket.gethostbyname(hostname)
+
 @app.get('/')
 async def endpoint():
     return {"Greetings" : "Welcome to Manlingua API"}
+
+@app.get('/get_objects')
+async def getItems():
+    objects = model.names
+    
+    object_names = []
+    
+    for i in objects:
+        object_names.append(objects[i])
+         
+    # print(object_names)
+    
+    curated_list = [
+        "person", "bicycle", "car", "motorcycle", "bench", "bird", "cat", "dog", "backpack", "umbrella",
+        "handbag", "tie", "suitcase", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+        "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+        "chair", "couch", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard",
+        "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+        "scissors", "hair drier", "toothbrush"
+    ]
+
+    # Filter original_list to keep only items in curated_list
+    filtered_list = [item for item in object_names if item in curated_list]
+    
+    return {"objects": filtered_list}
 
 @app.post('/predict')
 async def predict(file: UploadFile = File(...)):
@@ -25,9 +53,6 @@ async def predict(file: UploadFile = File(...)):
     # Perform detection on an image
     results = model.predict(img)
 
-    # Initialize a Counter to keep track of detected object counts
-    object_counts = Counter()
-
     predictions = []
 
     # Loop through the results and append each detection to the predictions list
@@ -37,16 +62,17 @@ async def predict(file: UploadFile = File(...)):
             object_name = result.names[class_id]  # Get object name from class ID
             predictions.append({
                 'class': object_name,  # Add the object name instead of just class ID
-                'confidence': float(detection.conf),  # Confidence score
-                'box': detection.xyxy.tolist()  # Bounding box coordinates
+                'confidence': float(detection.conf)  # Confidence score
             })
 
     # Return empty predictions if there are none
     if not predictions:
         print("No predictions made by the model")
         return {"error": "No predictions found"}
+    
+    print(predictions)
 
     return {'predictions': predictions}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='10.60.62.153', port=8000)
+    uvicorn.run(app, host=IP, port=8000)
